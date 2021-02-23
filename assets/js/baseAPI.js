54b41d6cde5d0b3cfd8e0b0ea6fc9071eaed7d14
sjx <1079536053@qq.com>
2021-02-23 20:07:33 +0800

    基础功能实现

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,37 @@
 // 自动拼接url地址
-// ajaxPrefillter是ajax发起请求之前先运行的函数
+// ajaxPrefillter是ajax发起请求之前先调用的函数
 // 在ajax发起请求之前，将url的路径完整拼接
 $.ajaxPrefilter(function (option) {
     option.url = "http://ajax.frontend.itheima.net" + option.url
-    console.log(option.url);
+    // 判断url路径中是否包含/my/路径，包含时需要验证请求头  
+    if (option.url.includes("/my/")) {
+        option.headers = {
+            // localStorage.getItem("token") =>localStorage.token
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+    // 给全部的ajax请求全部挂载身份核验函数，判断用户是否登录
+    // complete回调函数，无论是否成功都会执行
+    // 强制用户登录，未登录状态不允许进入后台系统
+    option.complete = function (res) {
+        // 判断res的responseJSON中的值
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            // 强制清除token
+            localStorage.removeItem("token")
+            // 强制跳转
+            location.href = "/login.html"
+        }
+    }
+
+
+
+
+    // option.success = function (res) {
+    //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+    //         // 强制清除token
+    //         localStorage.removeItem("token")
+    //         // 强制跳转
+    //         location.href = "/login.html"
+    //     }
+    // }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+$(function () {
+    getUserInfo();
+    // 点击退出系统按钮，询问用户是否退出系统
+    $("#btnLogout").on("click", function () {
+        // layui中的弹出层，弹出提示退出系统
+        layer.confirm('是否退出系统?', {
+            // 弹出层左侧的图标
+            icon: 3,
+            title: '提示'
+            // 回调函数，即用户点击确定后执行的函数
+        }, function (index) {
+            // 用户确定退出后，清除localStorage中的token
+            localStorage.removeItem("token")
+            // 跳转至login页面
+            location.href = "/login.html"
+            // 默认清除，保留即可
+            layer.close(index);
+        });
+    })
+})
+// 获取用户登录信息
+function getUserInfo() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        // ajax中的header验证在ajaxPrefilter中已经包含
+        success(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            // 渲染用户头像和用户名
+            renderAvatar(res.data)
+        },
+
+    })
+}
+
+function renderAvatar(user) {
+    // 判断登录用户是否有昵称，若没有昵称，则显示用户名
+    const username = user.nickname || user.username
+    // 将昵称或用户名渲染到页面中
+    $("#welcome").html("欢迎 " + username)
+    // 判断该账户是否有图片头像
+    if (user.picture) {
+        // 如果有头像，将头像图片渲染到layui-nav-img容器中，并显示
+        $(".layui-nav-img").attr("src", user.user_picture).show();
+        // 将文字头像隐藏
+        $(".text-avatar").hide()
+    } else {
+        // 账户没有图片头像
+        // 隐藏图片头像容器
+        $(".layui-nav-img").hide()
+        // 获取用户名的首字母，并将其大写
+        const firstName = username[0].toUpperCase();
+        // 将大写后的首字母渲染到页面中并显示
+        $(".text-avatar").html(firstName).show()
+    }
+}
